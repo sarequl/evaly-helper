9f6b7f8ac862f816d4ffaafcb323cb7dce6ab27e
Shahriar Shojib <shahriar_shojib@hotmail.com>
2020-06-18 02:47:51 +0600

    updated build config to minify build file

--- rollup.config.js
+++ rollup.config.js
@@ -2,37 +2,39 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
-
+import { terser } from 'rollup-plugin-terser';
 module.exports = {
-  input: 'svelte-src/index.js',
-  output: {
-    file: 'popup/dist/bundle.js',
-    format: 'iife',
-    name: 'app'
-  },
-  plugins: [
-    svelte({
-      emitCss: true
-    }),
-    resolve({
-      browser: true,
-      dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
-    }),
-    commonjs(),
-    postcss({
-      extract: true,
-      minimize: true,
-      use: [
-        ['sass', {
-          includePaths: [
-            './svelte-src/theme',
-            './node_modules'
-          ]
-        }]
-      ]
-    })
-  ],
-  watch: {
-    clearScreen: false
-  }
+	input: 'svelte-src/index.js',
+	output: {
+		file: 'popup/dist/bundle.js',
+		format: 'iife',
+		name: 'app'
+	},
+	plugins: [
+		svelte({
+			dev: false,
+			emitCss: true
+		}),
+		resolve({
+			browser: true,
+			dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
+		}),
+		commonjs(),
+		terser(),
+		postcss({
+			extract: true,
+			minimize: true,
+			use: [
+				['sass', {
+					includePaths: [
+						'./svelte-src/theme',
+						'./node_modules'
+					]
+				}]
+			]
+		})
+	],
+	watch: {
+		clearScreen: false
+	}
 };
